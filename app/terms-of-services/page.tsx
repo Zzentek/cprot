7fb1f3e7e039db845f9e5fb71f9c5068df7d4c0b
Zzentek <carlitos.hill.soltero@gmail.com>
2025-09-09 21:03:45 -0600

    update 09/09/25

--- app/terms-of-services/page.tsx
+++ app/terms-of-services/page.tsx
@@ -35,7 +35,7 @@ export default function TermsPage() {
           className="flex justify-center items-center"
         >
           <iframe
-            src="/Terminos de Servicio CPROT dc 12_08_25.pdf"
+            src="https://lobby.cprot.net/Terminos-Servicio-CPROT-12_08_25.pdf"
             className="w-full max-w-5xl h-[90vh] bg-white rounded-lg border-none shadow-lg"
           />
         </motion.div>
